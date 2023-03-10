9e1cbe550cd1c5c43b2fd2faed15342b09b1d676
Ivan Konishchev <kispaj@mail.ru>
2023-03-10 10:14:56 +0300

    Add change: main added property num and output to the console

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+// main.cpp
+// 2023-03-10
+// Ivan Konishchev
+//
+
+#include <iostream>
+int main(int argc, const char *argv[])
+{
+    int num{123};
+    std::cout << "Test Pull Request\n"
+              << "property: " << num
+              << std::endl;
+    return 0;
+}
